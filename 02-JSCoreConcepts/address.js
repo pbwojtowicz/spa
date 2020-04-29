5810b9ba8a7395b9c1a00ff4606ced02f3ad9796
pbwojtowicz <33104143+pbwojtowicz@users.noreply.github.com>
2020-04-29 11:57:24 +0200

    Add files via upload
    done

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -1,10 +1,10 @@
-title = "dr";
-name = "Mateusz";
-surname = 'Kubicki';
-street = 'Długa 15';
-city = 'Kraków';
-zip = '30-781';
-country = 'Poland';
+var title = "dr";
+var name = "Mateusz";
+var surname = 'Kubicki';
+var street = 'Długa 15';
+var city = 'Kraków';
+var zip = '30-781';
+var country = 'Poland';
 
 
 /* 
@@ -13,3 +13,15 @@ ul. Długa 15
 30-781 Kraków
 POLAND
 */
+console.log(title + ' ' + name + ' ' + surname + '\n' + 'ul. '+ street + '\n' + zip + ' ' + city + '\n' + country.toUpperCase());
+
+var str = `${title} ${name} ${surname} 
+ul. ${street} 
+${zip} ${country}`;
+
+console.log(str);
+
+console.log(`${title} ${name} ${surname}
+ul. ${street} 
+${zip} ${city}
+${country}`);
